b5da91cbff8361b45219120f1a5a91d5e3d55b16
MatteoRigamonti <matteo_rigamonti@hotmail.it>
2021-02-17 10:35:16 +0100

    Start animation added

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const colors = ["#FFBF00", "#FF7F50", "#DE3163", "#9FE2BF", "#40E0D0", "#6495ED", "#CCCCFF"];
+const colorsNumber = colors.length;
 const getBgColor = document.getElementById("general-bg-color");
+const getSquare = document.getElementById("square");
 
 const changeColor = () => {
-	const randomNumber = Math.round(Math.random() * colors.length);
+	const randomNumber = Math.round(Math.random() * colorsNumber);
 
-	getBgColor.style.backgroundColor = colors[randomNumber];
+	getSquare.style.width = "300vh";
+	getSquare.style.height = "300vh";
+	getSquare.style.backgroundColor = colors[randomNumber];
 };
